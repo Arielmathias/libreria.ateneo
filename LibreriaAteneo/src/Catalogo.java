77e64ba25a20eae8c3e23da62b7c35b0b82ff0c9
Ariel Olivares <ar.olivaresv@duocuc.cl>
2023-10-09 12:03:36 -0300

    Programa casi terminado

--- LibreriaAteneo/src/Catalogo.java
+++ LibreriaAteneo/src/Catalogo.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import java.util.ArrayList;
 
 /**
  *
  * @author arielmathias
  */
 public class Catalogo {
-    
-    
+
+    private ArrayList<Libro> catalogo = new ArrayList<>();
+
+    public ArrayList<Libro> getCatalogo() {
+        return catalogo;
+    }
+
+    public void setCatalogo(ArrayList<Libro> catalogo) {
+        this.catalogo = catalogo;
+    }
+
+    public void agregaLibro(Libro libro) {
+        this.catalogo.add(libro);
+    }
+
+    public void mostrarCatalogo() {
+        for (Libro libro : catalogo) {
+            libro.imprimirInfo();
+
+        }
+    }
+
+    public void eliminarLibroIsbn(String isbn) {
+        for (Libro libro : catalogo) {
+            if (libro.getIsbn().equalsIgnoreCase(isbn)) {
+                catalogo.remove(libro);
+                System.out.println("Libro eliminado con éxito ");
+                break;
+                
+            }
+
+        }
+    }
+
+    public void buscarLibroTitulo(String titulo) {
+        for (Libro libro : catalogo) {
+            if (libro.getTitulo().equalsIgnoreCase(titulo)) {
+                libro.imprimirInfo();
+            }
+
+        }
+    }
 }
