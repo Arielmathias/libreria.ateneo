6668328fbb2ce656c70581a24ffe4b83172ede0f
Ariel Olivares <ar.olivaresv@duocuc.cl>
2023-10-08 23:24:55 -0300

    Avance

--- LibreriaAteneo/src/Principal.java
+++ LibreriaAteneo/src/Principal.java
@@ -3,5 +3,14 @@
  * @author arielmathias
  */
 public class Principal {
+    
+    public static void main(String[] args) {
+        Libro harry = new Libro("Harry Potter", "JK Rowling", "1234", 20000);
+        
+        
+        harry.imprimirInfo();
+        System.out.println("IVA: "+harry.calcularIva());
+        
+    }
         
 }
