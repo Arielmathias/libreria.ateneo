89e8962f797fe277a539984b666c4c8243d8e164
Ariel Olivares <ar.olivaresv@duocuc.cl>
2023-10-08 22:40:31 -0300

    Atributos libro

---
+++ LibreriaAteneo/src/Libro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arielmathias
+ */
+public class Libro {
+    
+    private String titulo, autor;
+    private int precio, isbn;
+    
+    
+    
+}
